13d8131bdcd6809aba96da338b54b53af5baf462
moffy-Black <black.river.emperor@i.softbank.jp>
2021-04-20 23:17:46 +0900

    add: explaining comments

--- app/client_ipv4.py
+++ app/client_ipv4.py
@@ -11,9 +11,9 @@ recv_size = 1024
 message = sys.argv[1]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect(server_address)
-    client_socket.send(message.encode('utf-8'))
+    client_socket.connect(server_address) #サーバに接続
+    client_socket.send(message.encode('utf-8')) #dataを送信
 
-    data = client_socket.recv(recv_size)
+    data = client_socket.recv(recv_size) #返ってきたdataを取得
 
     print(data.decode('utf-8'))
